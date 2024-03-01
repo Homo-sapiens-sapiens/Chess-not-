205b919f7ba77e51f3924cbff7e4bcba59f90960
rafikharosh2 <161487719+rafikharosh2@users.noreply.github.com>
2024-03-01 19:08:34 +0300

    Add files via upload

---
+++ Script/CreateRoomManager.cs
@@ -0,0 +1,24 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreateRoomManager : MonoBehaviourPunCallbacks
+{
+    public void Back()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    //Метод фотон, который срабатывает при выходе
+    public override void OnLeftRoom()
+    {
+        //запускаем сцену с Меню игры
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Script/VolumeControl.cs
@@ -0,0 +1,21 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviourPunCallbacks
+{
+    public void Back()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    //Метод фотон, который срабатывает при выходе
+    public override void OnLeftRoom()
+    {
+        //запускаем сцену с Меню игры
+        SceneManager.LoadScene(0);
+    }
+}
